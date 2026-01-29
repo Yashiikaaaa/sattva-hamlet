0447e4dcc7516e9edc234fcad6e432a26f25bf0a
yashiikaaaa <yashika@truestate.in>
2026-01-29 17:51:20 +0530

    new chaznges

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -124,10 +124,10 @@ export const Navbar = ({ sitevisitmodal, setSiteVisitModal, setContactModal }) =
           {/* Call Button */}
           <div className="hidden lg:flex items-center">
             <a
-              href="tel:+919901963853"
+              href="tel:+919353329893"
               className="flex items-center bg-[#1f276b] text-white px-4 py-2 rounded-lg shadow-lg hover:bg-opacity-90 transition"
             >
-              <Phone className="w-5 h-5 mr-2" />99019 63853
+              <Phone className="w-5 h-5 mr-2" />93533 29893
             </a>
           </div>
         </div>
